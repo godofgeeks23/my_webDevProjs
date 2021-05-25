9e0a943bc70f5e1d593242cd64a3fb413cfa4515
Aviral Srivastava <aviralji4@gmail.com>
2021-05-25 22:58:54 +0530

    "updated the code base..."

---
+++ innogeeks_projs/javascript/task1/script.js
@@ -0,0 +1,32 @@
+console.clear()
+var country, continent, population
+country = "India"
+continent = "Asia"
+population = 1360000000
+console.log(country, continent, population)
+
+/**************************************** */
+
+var island = true
+var language
+console.log(island, population, country, language)
+
+/**************************************** */
+
+// language = "hindi"
+// delete country
+// const country = "india"
+
+/**************************************** */
+
+language = "hindi"
+var half_pop = population/2
+console.log(half_pop)
+half_pop += 1
+console.log(half_pop)
+var fin_pop = 6000000
+console.log(population>fin_pop)
+var avg_pop = 33000000
+console.log(population<avg_pop)
+var description = country + " is in " + continent + ", and its " + population + " speak " + language
+console.log(description)
