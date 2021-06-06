4ad131defc0d0f4bbed6937e5d4e813ed7c6c163
Aviral Srivastava <aviralji4@gmail.com>
2021-06-06 13:26:14 +0530

    "updated the code base..."

--- innogeeks_projs/javascript/basics5/script.js
+++ innogeeks_projs/javascript/basics5/script.js
@@ -25,17 +25,24 @@ console.time("Time started...");
 console.timeEnd("Time started...");
 
 const greeting = "Hello";
-greeting = "World";         // we cant do this, as const var values cant be changed
+// greeting = "World";         // we cant do this, as const var values cant be changed
 
-const testvar;      // this throws an error, because conts variables have to be initialized at the time of declaration.
+// const testvar;      // this throws an error, because conts variables have to be initialized at the time of declaration.
 
 const arr = [1,2,3,4,5];        // now this array cant be reassigned, but std array oeprations can still be performed
-arr = [1,2,3,4,56,6];           // this is not allowed
+// arr = [1,2,3,4,56,6];           // this is not allowed
 
 // but these things can be done
 arr.push(78);                   
 arr[2] = 789;
 
 // similarly for object also, except from declaring it completely, it can be modified ising its properties...
+
+const person = {
+    name: "Aviral",
+    age: 20
+}
+person.age = 21;        // we can still do this...
+person["surname"] = "srivastava";   // and this also..
 
 
