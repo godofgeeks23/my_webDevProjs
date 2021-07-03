62b75ef91717801fe15cf63b975b44798c8e14c6
Aviral Srivastava <aviralji4@gmail.com>
2021-07-04 00:56:19 +0530

    "updated the code base..."

---
+++ innogeeks_projs/javascript/4_learningAPI_0/script.js
@@ -0,0 +1,13 @@
+// fetch() can both retrieve(get()) and send(post())
+// fetch() is a asynchronous function
+// json - javascript object Notation 
+
+// the image file is fetched, then look at the resolved promise and convert it into a blob
+fetch('rainbow.jpg').then(response => {
+    console.log(response);
+    return response.blob();
+}).then(blob => {
+    console.log(blob);
+    // document.getElementById('rainbow').src = blob;  // the blob object is not in the format in which it is expected by the dom element
+    document.getElementById('rainbow').src = URL.createObjectURL(blob);
+});
