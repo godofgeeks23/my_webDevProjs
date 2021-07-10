c3061f7f6b31719a79635a255df186cebdc14253
Aviral Srivastava <aviralji4@gmail.com>
2021-07-10 09:54:35 +0530

    "updated the code base..."

--- aviBot/bot.js
+++ aviBot/bot.js
@@ -4,9 +4,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 require('dotenv').config();
-console.log(process.env.TOKEN);
 client.login(process.env.TOKEN);
 client.on('ready', readyDiscord);
+
+function readyDiscord() {
+    console.log("Ready!");
+}
 
 const replies = [
     "namaste",
@@ -16,40 +19,24 @@ const replies = [
     "bonjour"
 ];
 
-function readyDiscord()
-{
-    console.log("Ready!");
-}
-
 client.on('message', gotMessage);
 
-function gotMessage(msg)
-{
+function gotMessage(msg) {
     console.log(msg.content);
     // msg.channel.id      // return the id of the channel from which the message belongs
     // const index = Math.floor(Math.random()*replies.length);
-    if(msg.content.toLowerCase() == 'hello beta') 
-    {
+    if (msg.content.toLowerCase() == 'hello beta')
         msg.reply("hello papa");
-    }
-    if(msg.content.toLowerCase() == 'kaise ho beta') 
-    {
+    if (msg.content.toLowerCase() == 'kaise ho beta')
         msg.reply("thk hu papa... aap btao");
-    }
-    if(msg.content.toLowerCase() == 'm bhi thk hu beta') 
-    {
+    if (msg.content.toLowerCase() == 'm bhi thk hu beta')
         msg.reply("bdyaa papa");
-    }
-    if(msg.content.toLowerCase() == 'haso beta') 
-    {
+    if (msg.content.toLowerCase() == 'haso beta')
         msg.reply("😂😂");
-    }
-    if(msg.content.toLowerCase() == 'bye beta') 
-    {
+    if (msg.content.toLowerCase() == 'bye beta')
         msg.reply("bye bye papa👋");
-    }
-    
-    
+
+
 
 }
 
